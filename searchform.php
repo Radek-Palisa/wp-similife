5227a8386d67b249ad06435cff2ca61bfa4366ba
Radek Palisa <radek.palisa@gmail.com>
2017-03-13 23:24:35 +0100

    more work on search

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form action="<?php echo home_url( '/' ); ?>" method="get">
     <input type="text" name="s" id="search" class="search-text">
     <label for="search">Co vás zajímá?</label>
-    <input type="submit" class="search-submit">
+    <input type="submit" class="search-submit" value="" title="vyhledat">
 </form>
